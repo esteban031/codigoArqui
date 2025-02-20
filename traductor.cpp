c2703b3348ea181cdd5b6a83480b3d21d0523abd
Valentina Feijoo <val1015.fei@gmail.com>
2025-02-19 21:45:54 -0500

    Initial code

---
+++ traductor.cpp
@@ -0,0 +1,23 @@
+/* 
+Parte práctica parcial 1
+Traductor MIPS - Binario
+Integrantes: Valentina Feijoo y Esteban Arismendi 
+
+*/
+
+
+#include <map>
+#include <vector>
+#include <list>
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+
+int main(){
+    
+
+
+    return 0;
+}
